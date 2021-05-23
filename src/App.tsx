74d48ce3cb7fc86d3f9d9a3f3df813be717e2b1b
Wilson Faustino <wilson.rfaustino@gmail.com>
2021-05-23 20:50:08 -0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import {Provider, defaultTheme, Button, View, TextField} from '@adobe/react-spectrum';
-
+import React from 'react'
+import {
+    Provider,
+    defaultTheme,
+    Button,
+    View,
+    Flex
+} from '@adobe/react-spectrum'
 
 function App() {
-  return (
-    <Provider theme={defaultTheme}>
-      <View height='100vh' backgroundColor='indigo-400'>
-        <Button variant="cta" onPress={() => alert('Hey there!')}>
-          Hello React Spectrum!
-        </Button>
-      </View>
-    </Provider>
-  );
+    return (
+        <Provider theme={defaultTheme}>
+            <View height='100vh' backgroundColor='indigo-400'>
+                <Flex
+                    alignItems='center'
+                    justifyContent='center'
+                    height='100vh'
+                >
+                    <Button variant='cta' onPress={() => alert('Hey there!')}>
+                        Hello React Spectrum!
+                    </Button>
+                </Flex>
+            </View>
+        </Provider>
+    )
 }
 
-export default App;
+export default App
