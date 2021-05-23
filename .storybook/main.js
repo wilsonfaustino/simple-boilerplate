33487c32fdefe127aafa36a56143b58dee7e4555
Adeonir Kohl <adeonir@gmail.com>
2021-05-23 20:53:53 -0300

    Install eslint, prettier and husky

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,16 @@
+/* eslint-disable no-undef */
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  features: {
+    postcss: false,
+  },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  },
 }
---
+++ src/App.stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react'
+
+import App from './App'
+
+export default {
+  title: 'App',
+  component: App,
+} as Meta
+
+export const Primary: Story = () => <App />
