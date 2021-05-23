d6d3aa15bf66e64a94fc281cc89eddf5ada7b509
Wilson Faustino <wilson.rfaustino@gmail.com>
2021-05-23 20:24:34 -0300

    added eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render the button on app component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole('button', { name: /react spectrum/i } )
+  expect(buttonElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
 import React from 'react';
 import {Provider, defaultTheme, Button, View, TextField} from '@adobe/react-spectrum';
-import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
-const textState = atom({
-    key: 'textState',
-    default: ''
-})
-  
-  function TextInput() {
-    const [text, setText] = useRecoilState(textState);
-  
-    return (
-      <View>
-        <TextField label="Name" value={text} onChange={setText} />
-        <br />
-        Echo: {text}
-      </View>
-    );
-  }
-
-  const charCountState = selector({
-    key: 'charCountState', // unique ID (with respect to other atoms/selectors)
-    get: ({get}) => {
-      const text = get(textState);
-  
-      return text.length;
-    },
-  });
-  function CharacterCount() {
-    const count = useRecoilValue(charCountState);
-  
-    return <>Character Count: {count}</>;
-  }
 
 function App() {
   return (
     <Provider theme={defaultTheme}>
       <View height='100vh' backgroundColor='indigo-400'>
-        <TextInput />
-        <CharacterCount />
-        <br />
         <Button variant="cta" onPress={() => alert('Hey there!')}>
           Hello React Spectrum!
         </Button>
